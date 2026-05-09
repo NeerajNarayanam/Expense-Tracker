4d294c0144e3cb16d94a1c79d2e476216a49ccc9
crazyblaster18-dev <crazyblaster18@gmail.com>
2026-05-09 19:58:44 +0530

    Permanent Vercel CORS fix

--- backend/main.py
+++ backend/main.py
@@ -1,32 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from routers.expenses import (
-    router as expense_router
-)
-
-from routers.auth import (
-    router as auth_router
-)
-
-from routers.income import (
-    router as income_router
-)
+from routers.expenses import router as expense_router
+from routers.auth import router as auth_router
+from routers.income import router as income_router
 
 app = FastAPI()
 
-# CORS
 app.add_middleware(
     CORSMiddleware,
 
-    allow_origins=[
-
-        # LOCALHOST
-        "http://localhost:5173",
-
-        # CURRENT VERCEL FRONTEND
-        "https://expense-tracker-mauve.vercel.app",
-    ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
 
     allow_credentials=True,
 
@@ -35,18 +19,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# ROUTES
 app.include_router(auth_router)
-
 app.include_router(expense_router)
-
 app.include_router(income_router)
 
-# HOME
 @app.get("/")
 def home():
-
     return {
-        "message":
-        "Expense Tracker Backend Running"
+        "message": "Expense Tracker Backend Running"
     }
