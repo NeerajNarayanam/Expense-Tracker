ff0c4aad01c194b22c75e6b52b80f434d2aa3d82
crazyblaster18-dev <crazyblaster18@gmail.com>
2026-05-09 13:12:00 +0530

    Fixed Vercel production CORS issue

--- backend/main.py
+++ backend/main.py
@@ -1,29 +1,55 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from routers.expenses import router as expense_router
-from routers.auth import router as auth_router
-from routers.income import router as income_router
+from routers.expenses import (
+    router as expense_router
+)
+
+from routers.auth import (
+    router as auth_router
+)
+
+from routers.income import (
+    router as income_router
+)
 
 app = FastAPI()
 
-# CORS
+# CORS CONFIGURATION
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+
+    allow_origins=[
+        "http://localhost:5173",
+
+        "https://expense-tracker-sigma-coral-41.vercel.app",
+    ],
+
     allow_credentials=True,
+
     allow_methods=["*"],
+
     allow_headers=["*"],
 )
 
-# ROUTERS
-app.include_router(expense_router)
-app.include_router(auth_router)
-app.include_router(income_router)
+# ROUTES
+app.include_router(
+    expense_router
+)
 
+app.include_router(
+    auth_router
+)
+
+app.include_router(
+    income_router
+)
+
+# HOME ROUTE
 @app.get("/")
 def home():
 
     return {
-        "message": "Expense Tracker Backend Running"
+        "message":
+        "Expense Tracker Backend Running"
     }
